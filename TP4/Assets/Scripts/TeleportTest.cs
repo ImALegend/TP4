2f1e6a5b301c371dcb48c43f668e908826da8acf
ImALegend <56801294+ImALegend@users.noreply.github.com>
2019-12-02 17:40:48 -0500

    ajout de teleport

---
+++ TP4/Assets/Scripts/TeleportTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTest : MonoBehaviour
+{
+    [SerializeField]
+    Transform teleportTarget;
+    [SerializeField]
+    GameObject thePlayer;
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.name == "FirstPersonCharacter")
+            thePlayer.transform.position = teleportTarget.transform.position;
+    }
+}
